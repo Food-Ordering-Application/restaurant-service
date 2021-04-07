be88ea98f014e196a6f32270bd65e1b520b808fb
ADMIN\Admin <thachdau16t@gmail.com>
2021-04-07 12:12:29 +0700

    Test microservices with RabbitMQ transporter

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject('USER_SERVICE') private client: ClientProxy) {}
+
+  getHello(): Observable<string> {
+    return this.client.send<string>({ cmd: 'Hello' }, { name: 'hien' });
   }
 }
