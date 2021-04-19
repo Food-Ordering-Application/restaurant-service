e30eef8c100154f802b867f21a462478822e9a1a
ADMINAdmin <thachdau16t@gmail.com>
2021-04-19 17:56:30 +0700

    create seed data for restaurant-service

--- src/database/factories/restaurant.factory.ts
+++ src/database/factories/restaurant.factory.ts
@@ -7,22 +7,39 @@ define(Restaurant, (faker: typeof Faker) => {
   const id = faker.random.uuid();
   const owner = faker.random.uuid();
   const name = faker.name.findName();
-  const imageUrl = faker.image.imageUrl();
+  const phoneNumber = faker.phone.phoneNumber('0#########');
+  const coverImageUrl = faker.image.imageUrl();
+  const verifiedImageUrl = faker.image.imageUrl();
   const videoUrl = 'https://www.youtube.com/watch?v=PrJtiLE42PM&t=1s';
   const numRate = faker.random.number({ max: 999 });
   const rating = faker.random.number({ min: 0, max: 5 });
+  const address = faker.address.streetAddress();
+  const city = faker.address.city();
   const area = faker.address.city();
   const isActive = true;
+  const isVerified = true;
+  const latitude = faker.address.latitude();
+  const longtitude = faker.address.longitude();
+  const geom = {
+    type: 'Point',
+    coordinates: [parseFloat(longtitude), parseFloat(latitude)],
+  };
 
   const restaurant = new Restaurant();
   restaurant.id = id;
   restaurant.owner = owner;
   restaurant.name = name;
-  restaurant.imageUrl = imageUrl;
+  restaurant.phone = phoneNumber;
+  restaurant.coverImageUrl = coverImageUrl;
+  restaurant.verifiedImageUrl = verifiedImageUrl;
   restaurant.videoUrl = videoUrl;
   restaurant.numRate = numRate;
   restaurant.rating = rating;
+  restaurant.address = address;
+  restaurant.city = city;
+  restaurant.geom = geom;
   restaurant.area = area;
   restaurant.isActive = isActive;
+  restaurant.isVerified = isVerified;
   return restaurant;
 });
