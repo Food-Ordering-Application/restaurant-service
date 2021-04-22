d2b58c57a1e9a98b5eb424596ea8710614bc167e
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-22 13:56:55 +0700

    add merchant login dto

--- api-gateway/src/user/merchant/dto/index.ts
+++ api-gateway/src/user/merchant/dto/index.ts
@@ -1,3 +1,6 @@
 export { CreateMerchantDto } from './create-merchant/create-merchant.dto';
 export { CreateMerchantResponseDto } from './create-merchant/create-merchant-response.dto';
 export { CreateMerchantConflictResponseDto } from './create-merchant/create-merchant-conflict-response.dto';
+export { LoginMerchantResponseDto } from './login-merchant/login-merchant-response.dto';
+export { LoginMerchantDto } from './login-merchant/login-merchant.dto';
+export { LoginMerchantUnauthorizedResponseDto } from './login-merchant/login-merchant-unauthorized-response.dto';
---
+++ api-gateway/src/user/merchant/dto/login-merchant/login-merchant-response.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ILoginCustomerData } from 'src/user/customer/interfaces/login-customer-data.interface';
+
+export class LoginMerchantResponseDto {
+  @ApiProperty({ example: 200, description: 'Return status code' })
+  statusCode: number;
+  @ApiProperty({ example: 'Operation successs', type: 'string' })
+  message: string;
+  @ApiProperty({
+    example: {
+      user: {
+        id: '0c1df8c8-40b1-41b4-a897-702e09f1fd60',
+        username: 'merchant123',
+      },
+      access_token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFuaHlldWVtZSIsImlhdCI6MTYxODA2MjY0MSwiZXhwIjoxNjE4MTQ5MDQxfQ.l_r4Nufo7XW8KVvYnvvtYYwqTlfveTY7qEJwx9ByBFg',
+    },
+    nullable: true,
+  })
+  data: ILoginCustomerData;
+}
