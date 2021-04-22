6c0396ddf6caca5add35562b3ea4ffe3b35bb2f5
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-22 19:08:26 +0700

    add restaurant event queue to duplicate data in user service

--- src/restaurant/restaurant.module.ts
+++ src/restaurant/restaurant.module.ts
@@ -1,9 +1,49 @@
+import { Restaurant } from './entities/restaurant.entity';
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
+import { RESTAURANT_EVENT, RESTAURANT_SERVICE } from 'src/constants';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Restaurant]),
+    ClientsModule.registerAsync([
+      {
+        name: RESTAURANT_EVENT,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('AMQP_URL') as string],
+            queue: 'restaurant_event_queue', // TODO configService.get('RESTAURANT_EVENT_AMQP_QUEUE'),
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+      {
+        name: RESTAURANT_SERVICE,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get('AMQP_URL') as string],
+            queue: 'restaurant_queue', //configService.get('RESTAURANT_AMQP_QUEUE'),
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [RestaurantController],
   providers: [RestaurantService],
 })
-export class RestaurantModule {}
+export class RestaurantModule { }
