4100cae7a4ea89b768e2bd4283fc1054727ea3e4
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-22 13:22:35 +0700

    add create merchant dto

---
+++ api-gateway/src/user/merchant/dto/index.ts
@@ -0,0 +1,3 @@
+export { CreateMerchantDto } from './create-merchant/create-merchant.dto';
+export { CreateMerchantResponseDto } from './create-merchant/create-merchant-response.dto';
+export { CreateMerchantConflictResponseDto } from './create-merchant/create-merchant-conflict-response.dto';
