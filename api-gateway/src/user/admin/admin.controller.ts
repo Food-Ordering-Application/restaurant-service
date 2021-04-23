1699318ede91f01783c2a36c9a588ac2562506f6
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-23 18:16:23 +0700

    add admin login service to api gateway

--- api-gateway/src/user/admin/admin.controller.ts
+++ api-gateway/src/user/admin/admin.controller.ts
@@ -1,8 +1,13 @@
-
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { ApiBody, ApiInternalServerErrorResponse, ApiTags } from "@nestjs/swagger";
+import { AdminJwtAuthGuard } from './../../auth/guards/jwts/admin-jwt-auth.guard';
+import { Body, Controller, HttpCode, Post, Request, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiBody, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import { AuthService } from "src/auth/auth.service";
 import { InternalServerErrorResponseDto } from "src/shared/dto/internal-server-error.dto";
+import { AdminLocalAuthGuard } from './../../auth/guards/locals/admin-local-auth.guard';
 import { AdminService } from "./admin.service";
+import { LoginAdminResponseDto } from "./dto/login-admin/login-admin-response.dto";
+import { LoginAdminUnauthorizedResponseDto } from "./dto/login-admin/login-admin-unauthorized-response.dto";
+import { LoginAdminDto } from "./dto/login-admin/login-admin.dto";
 import { VerifyRestaurantDto } from './dto/verify-restaurant/verify-restaurant.dto';
 
 @ApiTags('admin')
@@ -11,17 +16,29 @@ import { VerifyRestaurantDto } from './dto/verify-restaurant/verify-restaurant.d
 export class AdminController {
 
   constructor(
-    private adminService: AdminService
+    private adminService: AdminService,
+    private authService: AuthService
   ) { }
+
+  // Đăng nhập Admin
+  @ApiOkResponse({ type: LoginAdminResponseDto })
+  @ApiUnauthorizedResponse({ type: LoginAdminUnauthorizedResponseDto })
+  @ApiBody({ type: LoginAdminDto })
+  @UseGuards(AdminLocalAuthGuard)
+  @HttpCode(200)
+  @Post('/login')
+  async loginAdmin(@Request() req): Promise<LoginAdminResponseDto> {
+    return this.authService.adminLogin(req.user);
+  }
 
   // @ApiOkResponse({ type: VerifyCustomerPhoneNumberResponseDto })
   // @ApiUnauthorizedResponse({
   //   type: VerifyCustomerPhoneNumberUnauthorizedResponseDto,
   // })
   @ApiBody({ type: VerifyRestaurantDto })
-  // @ApiBearerAuth()
+  @ApiBearerAuth()
   @HttpCode(200)
-  // @UseGuards(JwtAuthGuard, PoliciesGuard)
+  @UseGuards(AdminJwtAuthGuard)
   // @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Customer))
   @Post('/verify-restaurant')
   async verifyRestaurant(
@@ -29,4 +46,7 @@ export class AdminController {
   ) {
     return await this.adminService.verifyRestaurant(verifyRestaurantDto);
   }
+
+
+
 }
--- api-gateway/src/user/admin/admin.service.ts
+++ api-gateway/src/user/admin/admin.service.ts
@@ -3,12 +3,29 @@ import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { USER_SERVICE } from "src/constants";
 import { VerifyRestaurantDto } from "./dto/verify-restaurant/verify-restaurant.dto";
+import { IAdmin, IUserServiceResponse } from './interfaces';
 
 @Injectable()
 export class AdminService {
   constructor(
     @Inject(USER_SERVICE) private userServiceClient: ClientProxy,
   ) { }
+
+  async getAuthenticatedAdmin(username: string, password: string): Promise<IAdmin> {
+    const authenticatedAdminResponse: IUserServiceResponse = await this.userServiceClient
+      .send('getAuthenticatedAdmin', { username, password })
+      .toPromise();
+    const { message, user, status } = authenticatedAdminResponse;
+    if (status !== HttpStatus.OK) {
+      throw new HttpException(
+        {
+          message
+        },
+        status,
+      );
+    }
+    return user;
+  }
 
   async verifyRestaurant(verifyRestaurantDto: VerifyRestaurantDto) {
     const { restaurantId } = verifyRestaurantDto;
