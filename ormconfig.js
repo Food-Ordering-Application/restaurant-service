41ff7eefa5907518af0348fdef8a23ca789d5a29
Van Hiep <levanhiep.95.95@gmail.com>
2021-04-30 00:21:25 +0700

    config db:setup

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,19 @@
 module.exports = {
   type: 'postgres',
-  host: 'postgis',
+  host: 'ec2-52-21-252-142.compute-1.amazonaws.com',
   port: '5432',
-  username: 'admin',
-  password: 'admin',
-  database: 'restaurantservice',
+  username: 'jpuinibkcznakl',
+  password: '1cc282eb4bcfc8956f7b2c8fe1c77893ba16f2b37e5768525ff08539dd21e71f',
+  database: 'd68qc8jraebi16',
   entities: [__dirname + '/**/*.entity.{js,ts}'],
   migrations: [__dirname + '/**/*.migration.{js,ts}'],
   synchronize: true,
   seeds: [__dirname + '/**/seeders/**/*.seed{.ts,.js}'],
   factories: [__dirname + '/**/factories/**/*.factory{.ts,.js}'],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
