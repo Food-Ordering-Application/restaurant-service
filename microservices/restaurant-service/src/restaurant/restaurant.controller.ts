eba32176710183cc5e6532d56bc1fcbe9c1a9b33
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-02 18:16:23 +0700

    synchronize verified restaurant status

--- microservices/restaurant-service/src/restaurant/restaurant.controller.ts
+++ microservices/restaurant-service/src/restaurant/restaurant.controller.ts
@@ -1,10 +1,11 @@
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { GetSomeRestaurantDto, GetRestaurantInformationDto } from './dto';
 import { IRestaurantResponse, IRestaurantsResponse } from './interfaces';
 import { RestaurantService } from './restaurant.service';
 import { ICreateRestaurantResponse } from './interfaces/create-restaurant-response.interface';
+import { RestaurantProfileUpdatedEventPayload } from './events/restaurant-profile-updated.event';
 
 @Controller()
 export class RestaurantController {
@@ -31,5 +32,10 @@ export class RestaurantController {
     return this.restaurantService.getRestaurantInformation(
       getRestaurantInformationDto,
     );
+  }
+
+  @EventPattern({ event: 'restaurant_profile_updated' })
+  async handleRestaurantProfileUpdated(data: RestaurantProfileUpdatedEventPayload) {
+    return await this.restaurantService.handleRestaurantProfileUpdated(data);
   }
 }
