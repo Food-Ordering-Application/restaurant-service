84077120be5554a58b96a0827968bc21d1414f0a
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-04 20:27:45 +0700

    remove staff functions from restaurant controller and service

--- api-gateway/src/user/merchant/restaurant/restaurant.controller.ts
+++ api-gateway/src/user/merchant/restaurant/restaurant.controller.ts
@@ -1,14 +1,12 @@
-import {
-  Body, Controller, Delete, Get, Logger, Param, Patch, Post, Query, Req, Request, UseGuards
-} from '@nestjs/common';
+import { Controller, Get, Logger, Param, Post, Query, Req, Request, UseGuards } from '@nestjs/common';
 import { Payload } from '@nestjs/microservices';
 import {
   ApiBearerAuth,
   ApiBody,
-  ApiConflictResponse,
-  ApiCreatedResponse,
+
+
   ApiInternalServerErrorResponse,
-  ApiNotFoundResponse,
+
   ApiOkResponse,
   ApiQuery,
   ApiTags,
@@ -18,12 +16,10 @@ import { MerchantJwtRequest } from 'src/auth/strategies/jwt-strategies/merchant-
 import { InternalServerErrorResponseDto } from '../../../shared/dto/internal-server-error.dto';
 import { MerchantJwtAuthGuard } from './../../../auth/guards/jwts/merchant-jwt-auth.guard';
 import { MerchantJwtPayload } from './../../../auth/strategies/jwt-strategies/merchant-jwt-payload.interface';
-import { DeleteStaffNotFoundResponseDto, DeleteStaffResponseDto, UpdateStaffDto, UpdateStaffResponseDto } from './dto';
-import { CreateStaffConflictResponseDto, CreateStaffDto, CreateStaffResponseDto, PaginationDto, FetchStaffByMerchantResponseDto, FetchStaffByMerchantUnauthorizedResponseDto } from './dto/';
+import { FetchRestaurantDto } from './dto';
 import { CreateRestaurantDto } from './dto/create-restaurant/create-restaurant.dto';
 import { FetchRestaurantsOfMerchantResponseDto } from './dto/fetch-restaurant/fetch-restaurant-response.dto';
 import { FetchRestaurantsOfMerchantUnauthorizedResponseDto } from './dto/fetch-restaurant/fetch-restaurant-unauthorized-response.dto';
-import { UpdateStaffNotFoundResponseDto } from './dto/update-staff/update-staff-not-found-response.dto';
 import { RestaurantService } from './restaurant.service';
 
 @ApiTags('merchant/restaurant')
@@ -36,108 +32,15 @@ export class RestaurantController {
     private restaurantService: RestaurantService,
   ) { }
 
-  // Tao nhan vien
-  @ApiCreatedResponse({ type: CreateStaffResponseDto })
-  @ApiConflictResponse({ type: CreateStaffConflictResponseDto })
-  @ApiBody({ type: CreateStaffDto })
-  @UseGuards(MerchantJwtAuthGuard)
-  @Post(':restaurantId/staff')
-  async createStaff(
-    @Request() req: MerchantJwtRequest,
-    @Param('merchantId') merchant,
-    @Param('restaurantId') restaurant,
-    @Body() createStaffDto: CreateStaffDto,
-  ): Promise<CreateStaffResponseDto> {
-    const { user } = req;
-    const { merchantId } = user;
-    if (merchantId !== merchant) {
-      return {
-        statusCode: 403,
-        message: 'Unauthorized',
-        data: null,
-      };
-    }
-    return await this.restaurantService.createStaff(merchantId, restaurant, createStaffDto);
-  }
-
-  // Update nhan vien
-  @ApiOkResponse({ type: UpdateStaffResponseDto })
-  @ApiNotFoundResponse({ type: UpdateStaffNotFoundResponseDto })
-  @ApiBody({ type: UpdateStaffDto })
-  @UseGuards(MerchantJwtAuthGuard)
-  @Patch(':restaurantId/staff/:staffId')
-  async updateStaff(
-    @Request() req: MerchantJwtRequest,
-    @Param('merchantId') merchant,
-    @Param('restaurantId') restaurant,
-    @Param('staffId') staff,
-    @Body() updateStaffDto: UpdateStaffDto,
-  ): Promise<UpdateStaffResponseDto> {
-    const { user } = req;
-    const { merchantId } = user;
-    if (merchantId !== merchant) {
-      return {
-        statusCode: 403,
-        message: 'Unauthorized',
-      };
-    }
-    return await this.restaurantService.updateStaff(staff, merchantId, restaurant, updateStaffDto);
-  }
-
-  // Delete nhan vien
-  @ApiOkResponse({ type: DeleteStaffResponseDto })
-  @ApiNotFoundResponse({ type: DeleteStaffNotFoundResponseDto })
-  @UseGuards(MerchantJwtAuthGuard)
-  @Delete(':restaurantId/staff/:staffId')
-  async deleteStaff(
-    @Request() req: MerchantJwtRequest,
-    @Param('merchantId') merchant,
-    @Param('restaurantId') restaurant,
-    @Param('staffId') staff,
-  ): Promise<DeleteStaffResponseDto> {
-    const { user } = req;
-    const { merchantId } = user;
-    if (merchantId !== merchant) {
-      return {
-        statusCode: 403,
-        message: 'Unauthorized',
-      };
-    }
-    return await this.restaurantService.deleteStaff(staff, merchantId, restaurant);
-  }
-
-  @ApiOkResponse({ type: FetchStaffByMerchantResponseDto })
-  @ApiUnauthorizedResponse({ type: FetchStaffByMerchantUnauthorizedResponseDto })
-  @ApiQuery({ type: PaginationDto, required: false })
-  @UseGuards(MerchantJwtAuthGuard)
-  @Get(':restaurantId/staff')
-  async fetchStaff(
-    @Request() req: MerchantJwtRequest,
-    @Param('merchantId') merchant,
-    @Param('restaurantId') restaurant,
-    @Query() fetchStaffByMerchantDto: PaginationDto,
-  ): Promise<FetchStaffByMerchantResponseDto> {
-    const { user } = req;
-    const { merchantId } = user;
-    if (merchantId !== merchant) {
-      return {
-        statusCode: 403,
-        message: 'Unauthorized',
-        data: null,
-      };
-    }
-    return await this.restaurantService.fetchStaff(merchantId, restaurant, fetchStaffByMerchantDto);
-  }
-
   @ApiOkResponse({ type: FetchRestaurantsOfMerchantResponseDto })
   @ApiUnauthorizedResponse({ type: FetchRestaurantsOfMerchantUnauthorizedResponseDto })
-  @ApiQuery({ type: PaginationDto, required: false })
+  @ApiQuery({ type: FetchRestaurantDto, required: false })
   @UseGuards(MerchantJwtAuthGuard)
   @Get()
   async fetchRestaurantsOfMerchant(
     @Request() req: MerchantJwtRequest,
     @Param('merchantId') merchant,
-    @Query() fetchStaffByMerchantDto: PaginationDto,
+    @Query() fetchRestaurantByMerchantDto: FetchRestaurantDto,
   ): Promise<FetchRestaurantsOfMerchantResponseDto> {
     const { user } = req;
     const { merchantId } = user;
@@ -148,7 +51,7 @@ export class RestaurantController {
         data: null,
       };
     }
-    return await this.restaurantService.fetchRestaurantsOfMerchant(merchantId, fetchStaffByMerchantDto);
+    return await this.restaurantService.fetchRestaurantsOfMerchant(merchantId, fetchRestaurantByMerchantDto);
   }
 
   @ApiBody({ type: CreateRestaurantDto })
--- api-gateway/src/user/merchant/restaurant/restaurant.service.ts
+++ api-gateway/src/user/merchant/restaurant/restaurant.service.ts
@@ -1,20 +1,9 @@
-import { ISimpleResponse } from './../../../shared/interfaces/simple-response.interface';
 import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import * as constants from '../../../constants';
-import {
-  CreateStaffDto,
-  CreateStaffResponseDto,
-  DeleteStaffResponseDto,
-  FetchStaffByMerchantResponseDto,
-  PaginationDto,
-  UpdateStaffDto,
-  UpdateStaffResponseDto
-} from './dto';
-import { IUserServiceCreateStaffResponse, IUserServiceFetchStaffByMerchantResponse } from './interfaces';
 import { CreateRestaurantDto } from './dto/create-restaurant/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dto/update-restaurant/update-restaurant.dto';
 import { FetchRestaurantsOfMerchantResponseDto } from './dto/fetch-restaurant/fetch-restaurant-response.dto';
+import { FetchRestaurantDto } from './dto/fetch-restaurant/fetch-restaurant.dto';
 import { IUserServiceFetchRestaurantsOfMerchantResponse } from './interfaces/user-service-fetch-restaurants-of-merchant-response.interface';
 
 @Injectable()
@@ -23,83 +12,6 @@ export class RestaurantService {
     @Inject(constants.USER_SERVICE) private userServiceClient: ClientProxy,
     @Inject(constants.RESTAURANT_SERVICE) private restaurantServiceClient: ClientProxy,
   ) { }
-
-  async createStaff(merchantId: string, restaurantId: string, createStaffDto: CreateStaffDto): Promise<CreateStaffResponseDto> {
-    const createStaffResponse: IUserServiceCreateStaffResponse = await this.userServiceClient
-      .send('createStaff', { merchantId, restaurantId, data: createStaffDto })
-      .toPromise();
-
-    const { status, message, data } = createStaffResponse;
-    if (status !== HttpStatus.CREATED) {
-      throw new HttpException({ message, }, status,);
-    }
-    const { staff } = data;
-    return {
-      statusCode: 201,
-      message,
-      data: {
-        staff
-      }
-    };
-  }
-
-  async updateStaff(staffId: string, merchantId: string, restaurantId: string, updateStaffDto: UpdateStaffDto): Promise<UpdateStaffResponseDto> {
-    const updateStaffResponse: ISimpleResponse = await this.userServiceClient
-      .send('updateStaff', { staffId, merchantId, restaurantId, data: updateStaffDto })
-      .toPromise();
-
-    const { status, message } = updateStaffResponse;
-    if (status !== HttpStatus.OK) {
-      throw new HttpException({ message, }, status,);
-    }
-
-    return {
-      statusCode: HttpStatus.OK,
-      message,
-    };
-  }
-
-  async deleteStaff(staffId: string, merchantId: string, restaurantId: string): Promise<DeleteStaffResponseDto> {
-    const deleteStaffResponse: ISimpleResponse = await this.userServiceClient
-      .send('deleteStaff', { staffId, merchantId, restaurantId })
-      .toPromise();
-
-    const { status, message } = deleteStaffResponse;
-    if (status !== HttpStatus.OK) {
-      throw new HttpException({ message, }, status,);
-    }
-
-    return {
-      statusCode: HttpStatus.OK,
-      message,
-    };
-  }
-
-  async fetchStaff(merchantId: string, restaurantId: string, fetchStaffByMerchantDto: PaginationDto): Promise<FetchStaffByMerchantResponseDto> {
-    const fetchStaffResponse: IUserServiceFetchStaffByMerchantResponse = await this.userServiceClient
-      .send('fetchStaff', {
-        merchantId,
-        restaurantId,
-        page: parseInt(fetchStaffByMerchantDto.page) || 0,
-        size: parseInt(fetchStaffByMerchantDto.size) || 10
-      })
-      .toPromise();
-
-    const { status, message, data } = fetchStaffResponse;
-    if (status !== HttpStatus.OK) {
-      throw new HttpException({ message, }, status,);
-    }
-    const { results, size, total } = data;
-    return {
-      statusCode: 200,
-      message,
-      data: {
-        results,
-        size,
-        total
-      }
-    };
-  }
 
   async createRestaurant(merchantId: string, createRestaurantDto: CreateRestaurantDto) {
     const isMerchantIdValid: boolean = await this.userServiceClient
@@ -125,7 +37,7 @@ export class RestaurantService {
     };
   }
 
-  async fetchRestaurantsOfMerchant(merchantId: string, fetchRestaurantsOfMerchantDto: PaginationDto): Promise<FetchRestaurantsOfMerchantResponseDto> {
+  async fetchRestaurantsOfMerchant(merchantId: string, fetchRestaurantsOfMerchantDto: FetchRestaurantDto): Promise<FetchRestaurantsOfMerchantResponseDto> {
     const fetchRestaurantsOfMerchantResponse: IUserServiceFetchRestaurantsOfMerchantResponse
       = await this.userServiceClient
         .send('fetchRestaurantsOfMerchant', {
