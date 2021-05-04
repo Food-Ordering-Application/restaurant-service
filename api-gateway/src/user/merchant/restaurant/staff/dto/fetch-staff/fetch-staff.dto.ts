5a9ebecb2cfb77e10c55449c68db0cef36feea22
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-04 20:22:04 +0700

    move staff dto to staff directory

---
+++ api-gateway/src/user/merchant/restaurant/staff/dto/fetch-staff/fetch-staff.dto.ts
@@ -0,0 +1,5 @@
+import { PaginationDto } from '../../../../../../shared/dto/pagination.dto';
+
+export class FetchStaffDto extends PaginationDto {
+}
+
---
+++ api-gateway/src/user/merchant/restaurant/staff/dto/index.ts
@@ -0,0 +1,11 @@
+export { CreateStaffConflictResponseDto } from './create-staff/create-staff-conflict-response.dto';
+export { CreateStaffResponseDto } from './create-staff/create-staff-response.dto';
+export { CreateStaffDto } from './create-staff/create-staff.dto';
+export { DeleteStaffNotFoundResponseDto } from './delete-staff/delete-staff-not-found-response.dto';
+export { DeleteStaffResponseDto } from './delete-staff/delete-staff-response.dto';
+export { FetchStaffByMerchantResponseDto } from './fetch-staff/fetch-staff-response.dto';
+export { FetchStaffByMerchantUnauthorizedResponseDto } from './fetch-staff/fetch-staff-unauthorized-response.dto';
+export { FetchStaffDto } from './fetch-staff/fetch-staff.dto';
+export { UpdateStaffNotFoundResponseDto } from './update-staff/update-staff-not-found-response.dto';
+export { UpdateStaffResponseDto } from './update-staff/update-staff-response.dto';
+export { UpdateStaffDto } from './update-staff/update-staff.dto';
