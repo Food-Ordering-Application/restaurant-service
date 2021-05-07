fb7a1c3fe6adf0176443bd38433b51b42e4fcacd
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-07 22:31:33 +0700

    add menu item dto in ResSvc

---
+++ src/menu/menu-item/dto/menu-item.dto.ts
@@ -0,0 +1,16 @@
+import { MenuItem } from '../../entities/menu-item.entity';
+export class MenuItemDto {
+  id: string;
+  menuId: string;
+  menuGroupId: string;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  isActive: boolean;
+  index: number;
+  static EntityToDto(menu: MenuItem): MenuItemDto {
+    const { id, menuId, menuGroupId, name, description, price, imageUrl, isActive, index } = menu;
+    return { id, menuId, menuGroupId, name, description, price, imageUrl, isActive, index };
+  }
+}
