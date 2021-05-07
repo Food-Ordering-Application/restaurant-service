3b7ea7e4f48d82345b563d8ae7df74122e9b3985
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-07 22:32:42 +0700

    add menu item service and controller in ResSvc

--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Menu, MenuGroup, MenuItem, ToppingGroup } from './entities';
 import { MenuGroupService } from './menu-group/menu-group.service';
 import { MenuGroupController } from './menu-group/menu-group.controller';
+import { MenuItemService } from './menu-item/menu-item.service';
+import { MenuItemController } from './menu-item/menu-item.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Menu, MenuGroup, MenuItem, ToppingGroup]),
   ],
-  controllers: [MenuController, MenuGroupController],
-  providers: [MenuService, MenuGroupService],
+  controllers: [MenuController, MenuGroupController, MenuItemController],
+  providers: [MenuService, MenuGroupService, MenuItemService],
 })
 export class MenuModule { }
