1ede9da4f2000f6e78c5b048f43d22dc7e96a238
mrjared16 <doannguyentanphuc99@gmail.com>
2021-05-08 23:53:04 +0700

    add topping item CRUD response interface

---
+++ src/menu/topping-item/interfaces/create-topping-item-of-menu-response.interface.ts
@@ -0,0 +1,9 @@
+import { ToppingItemDto } from '../dto/topping-item.dto';
+
+export interface ICreateToppingItemResponse {
+  status: number;
+  message: string;
+  data: {
+    toppingItem: ToppingItemDto;
+  }
+}
